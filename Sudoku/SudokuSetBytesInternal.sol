1cfe7930b4b6523d7ac9dd6bc9d72f1b633235ff
thecodeslide <45506833+thecodeslide@users.noreply.github.com>
2024-04-07 07:54:02 -0300

    library , isvalid and validblocks

--- Sudoku/SudokuSetBytesInternal.sol
+++ Sudoku/SudokuSetBytesInternal.sol
@@ -17,18 +17,39 @@ library SetSudokuLib {
   error duplicateError(bytes1, bytes32 , bytes4);
   error duplicateError2(bytes1, bytes4);
 
-  function insert(Set memory set, uint key, bytes32 action, bytes1 cellValue) internal pure {
-    bytes1 test = contains(set, cellValue);
-    if (contains(set, cellValue) == hex'01') revert duplicateError(bytes1(uint8(cellValue) + 1), action, hex'DEADBEEF');
+  function insert(Set storage set, uint key, bytes32 action, bytes1 cellValue) internal {
+    if (contains(set, cellValue) == hex'01') {
+      assembly {
+        let error := mload(0x40)
+        mstore(error, hex"011d9462")
+        mstore(add(error, 0x4) , cellValue)
+        mstore(add(error, 0x24), action)
+        mstore(add(error, 0x44), hex"DEADBEEF")
+        revert(error, 0x64)
+      }
+    } 
     set.values[uint(uint8(cellValue))] = hex'01';
   }
 
-  function contains(Set memory set, bytes1 cellValue) internal pure returns(bytes1) {
-    return set.values[uint(uint8(cellValue))];
+  function contains(Set storage set, bytes1 cellValue) internal returns(bytes1 result) {
+    assembly {
+      if and( sload(set.slot) , shl(mul(shr(248, cellValue), 8), 1)) {
+        result := hex"01"
+      }
+    }
   }
 
    function reset(Set storage set) internal {
-      delete set.values;
+    assembly {
+      sstore(set.slot, not(not(0)))
+
+      if and(sload(set.slot), 1) {
+        let mem := mload(0x40)
+        mstore(mem, hex"4e487b71")
+        mstore(add(mem, 0x4), 1)
+        revert(mem, 0x24)
+      }
+    }
     }
 }
 
@@ -40,82 +61,128 @@ contract Sudoku {
   SetSudokuLib.Set seenList;
   event Log(string indexed message);
 
-  function isValid(uint[INDEX][INDEX] calldata sudokuBoard) external pure returns (uint) {
+  function isValid(uint[INDEX][INDEX] calldata sudokuBoard) external returns (uint) {
     // TODO
     // if (!isValidRowsAndColumns(sudokuBoard)) {
     //     return false;
     // }
 
-    uint cellValue;
-
-    SetSudokuLib.Set memory rowList;
-    SetSudokuLib.Set memory colList;
-    SetSudokuLib.Set memory blockList;
-  
-    for (uint r = 0; r < 9; r++) {
-        for(uint c = 0; c < 9; c++) { // replacie with function insertBlockInner
-            cellValue = sudokuBoard[r][c];
-            if(cellValue != 0) {
-                cellValue = sudokuBoard[r][c] -1; // index
-                if(rowList.values[cellValue] == hex'01') {
-                    revert SetSudokuLib.duplicateError2(bytes1(uint8(cellValue)+1), hex'DEADBEEF') ;
-                }
-                rowList.values[cellValue] = hex'01';
+    assembly {
+      for { let r := 0} lt(r, 9) {r := add(r,1)} {
+        for { let c := 0} lt(c, 9) {c := add(c,1)} {
+          let cellValue := calldataload(add(add(mul(0x120, r), sudokuBoard) , mul(0x20, c))) // rows
+          if and(gt(cellValue, 0), lt(cellValue, 10)) {
+            cellValue := sub(cellValue, 1)
+            let val := sload(seenList.slot)
+            if and(shl(mul(cellValue, 8), 0xff) , val) {
+              let mem := mload(0x40)
+              mstore(mem, hex"f3175e8b")
+              mstore(add(mem, 0x4), shl(mul(8, 31), add(cellValue,1)))
+              mstore(add(mem, 0x24) , hex"deadbeef")
+              revert(mem, 0x44)
             }
+            sstore(seenList.slot, or(val, shl(mul(cellValue, 8), 1)))
+          }
 
-            cellValue = sudokuBoard[c][r]; // index
-            if(cellValue != 0) {
-                cellValue -= 1;
-                if(colList.values[cellValue] == hex'01') {
-                    revert SetSudokuLib.duplicateError2(bytes1(uint8(cellValue) +1),  hex'FDFDFDFD') ;
-                }
-                colList.values[cellValue] = hex'01';
-            }
+          // cols
+          cellValue := calldataload(add(add(mul(0x120, c), sudokuBoard) , mul(0x20, r))) 
+          if and(gt(cellValue, 0), lt(cellValue, 10)) {
+            cellValue := add(cellValue, 0x9)
+            let val := sload(seenList.slot)
 
-            cellValue = sudokuBoard[3* (r / 3 )+ c/3][3*(r%3)+(c%3)];
-            if(cellValue != 0) {
-                cellValue -= 1;
-                if(blockList.values[cellValue] == hex'01') {
-                    revert SetSudokuLib.duplicateError2(bytes1(uint8(cellValue) +1), hex'BEBEBEBE') ;
-                }
-                blockList.values[cellValue] = hex'01';
+            if and(shl(mul(cellValue, 8), 0xff) , val) { 
+              let mem := mload(0x40)
+              mstore(mem, hex"f3175e8b")
+              mstore(add(mem, 0x4), shl(mul(8, 31), sub(cellValue, 0x9)))
+              mstore(add(mem, 0x24) , hex"fdfdfdfd")
+              revert(mem, 0x44)
             }
+            sstore(seenList.slot, or(val, shl(mul(cellValue, 8), 1))) 
+          }
+
+          // block
+          let i := add(mul(3, div(r, 3)) , div(c, 3))
+          let j := add(mul(mod(r , 3), 3), mod(c, 3))
+          cellValue := calldataload(add(add(mul(0x120, i), sudokuBoard) , mul(0x20, j))) 
+          if and(gt(cellValue, 0), lt(cellValue, 10)) {
+            cellValue := add(cellValue, 0x13)
+            // cellValue := add(sub(cellValue, 1), 0x14)
+            let val := sload(seenList.slot)
+
+            if and(shl(mul(cellValue, 8), 0xff) , val) {
+              let mem := mload(0x40)
+              mstore(mem, hex"f3175e8b")
+              mstore(add(mem, 0x4), shl(mul(8, 31), sub(cellValue,0x13)))
+              mstore(add(mem, 0x24) , hex"bebebebe")
+              revert(mem, 0x44)
+            }
+            sstore(seenList.slot, or(val, shl(mul(cellValue, 8), 1)))
+          }
+          
+          if gt(cellValue, 9) {
+            let mem := mload(0x40)
+            mstore(mem, hex"08c379a0")
+            mstore(add(mem, 0x4), 0x20)
+            mstore(add(mem, 0x24), 0xf)
+            mstore(add(mem, 0x44), "number too high")
+            revert(mem, 0x64)
+          }
+        } 
+        // clear slot
+        sstore(seenList.slot, not(not(0)))
+        if and(sload(seenList.slot), 1) {
+          let mem := mload(0x40)
+          mstore(mem, hex"4e487b71") //panic
+          mstore(add(mem, 0x4), 1)
+          revert(mem, 0x24)
         }
-
-        delete rowList.values;
-        delete colList.values;
-        delete blockList.values;
-    }
+      }
+    } // endasm
 
     return 2; // true
   }
 
-  function isValidBlocks(uint[INDEX][INDEX] calldata sudokuBoard) external pure {
-    SetSudokuLib.Set memory seenListMem;
-    uint blockNumber = 0;
-    uint count = 0; // for dev. can be removed
+  function isValidBlocks(uint[INDEX][INDEX] calldata sudokuBoard) external {
     bytes1 cellValue;
 
-    for (uint rowBlock = 0; rowBlock < 9; rowBlock += 3) {
-      for (uint colBlock = 0; colBlock < 9; colBlock += 3) {
-        for (uint miniRow = rowBlock; miniRow < rowBlock + 3; miniRow++) {
-          for (uint miniCol = colBlock; miniCol < colBlock + 3; miniCol++) {
-            cellValue = bytes1(uint8(sudokuBoard[miniRow][miniCol]));
-            if (cellValue == 0) {
-              continue;
+    assembly {
+      for {let rowBlock := 0 } lt(rowBlock , 9) { rowBlock := add(rowBlock, 3) } {
+        for { let colBlock := 0 } lt(colBlock , 9) { colBlock := add(colBlock, 3) } {
+          let r := add(rowBlock , 3)
+          let c := add(colBlock, 3)
+          for { let miniRow := rowBlock } lt(miniRow, r) { miniRow := add(miniRow, 1) } {
+            for { let miniCol := colBlock } lt(miniCol, c) { miniCol := add(miniCol, 1) } {
+
+              cellValue := calldataload(add(add(mul(miniRow, 0x120), sudokuBoard), mul(miniCol, 0x20)))
+
+              if and(gt(cellValue, 0), lt(cellValue, 10)) {
+                let val := sload(seenList.slot)
+                if and(shl(mul(cellValue, 8), 0xff) , val) {
+                  let mem := mload(0x40)
+                  mstore(mem, hex"f3175e8b") // duplicate error2
+                  mstore(add(mem, 0x4), shl(mul(8, 31), cellValue))
+                  mstore(add(mem, 0x24) , hex"bebebebe")
+                  revert(mem, 0x44)
+                }
+                
+                sstore(seenList.slot, or(val, shl(mul(cellValue, 8), 1)))  
+              }
             }
-            seenListMem.insert(count++, "blocks", bytes1(uint8(cellValue) - 1));
+          }
+          sstore(seenList.slot, not(not(0)))
+          if and(sload(seenList.slot), 1) {
+            let mem := mload(0x40)
+            mstore(mem, hex"4e487b71") //panic
+            mstore(add(mem, 0x4), 1)
+            revert(mem, 0x24)
           }
         }
-        count = 0; // for dev. can be removed
-        blockNumber++;
-        delete seenListMem.values;
       }
-    }
+    } // endasm
     // emit Log("blocks");
   }
 
-  function isValidRows(uint[9][9] calldata sudokuBoard) external pure returns (uint) {
+  function isValidRows(uint[9][9] calldata sudokuBoard) external returns (uint) {
     SetSudokuLib.Set memory seenListMem;
 
     for (uint row = 0; row < 9; row++) {
@@ -125,7 +192,7 @@ contract Sudoku {
     return 2;
   }
 
-  function isValidColumns(uint[9][9] calldata sudokuBoard) external pure returns (uint) {
+  function isValidColumns(uint[9][9] calldata sudokuBoard) external returns (uint) {
     SetSudokuLib.Set memory seenListMem;
 
     for (uint col = 0; col < 9; col++) {
